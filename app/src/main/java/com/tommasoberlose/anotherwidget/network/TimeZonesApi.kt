1d0f7e69c77108fd03b6a8438bc44daf0e852933
masterramy <ramy.baheeg@gmail.com>
2026-09-13 00:42:05 -0700

    Q1: make timezone network response handling exhaustive

--- app/src/main/java/com/tommasoberlose/anotherwidget/network/TimeZonesApi.kt
+++ app/src/main/java/com/tommasoberlose/anotherwidget/network/TimeZonesApi.kt
@@ -3,19 +3,9 @@ package com.tommasoberlose.anotherwidget.network
 import android.content.Context
 import android.util.Log
 import com.chibatching.kotpref.Kotpref
-import com.google.gson.internal.LinkedTreeMap
 import com.haroldadmin.cnradapter.NetworkResponse
-import com.tommasoberlose.anotherwidget.R
-import com.tommasoberlose.anotherwidget.global.Constants
-import com.tommasoberlose.anotherwidget.global.Preferences
-import com.tommasoberlose.anotherwidget.helpers.WeatherHelper
 import com.tommasoberlose.anotherwidget.network.repository.TimeZonesRepository
-import com.tommasoberlose.anotherwidget.ui.fragments.MainFragment
-import com.tommasoberlose.anotherwidget.ui.widgets.MainWidget
-import org.greenrobot.eventbus.EventBus
 import java.lang.Exception
-import java.text.SimpleDateFormat
-import java.util.*
 
 class TimeZonesApi(val context: Context) {
     suspend fun getTimeZone(lat: String, long: String): String? {
@@ -26,13 +16,13 @@ class TimeZonesApi(val context: Context) {
         when (val response = repository.getTimeZone(lat, long)) {
             is NetworkResponse.Success -> {
                 try {
-                    Log.d("ciao", response.body.toString())
+                    Log.d("TimeZonesApi", response.body.toString())
                     id = response.body["timezoneId"] as String
-
-                } catch(ex: Exception) {
+                } catch (ex: Exception) {
                     ex.printStackTrace()
                 }
             }
+            else -> Unit
         }
 
         return id
